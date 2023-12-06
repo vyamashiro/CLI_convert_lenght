1f790493677ba78efc5afcebf282116fa6527326
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2023-12-06 23:00:05 +0000

    draft version

---
+++ src/convert_px.rs
@@ -0,0 +1,6 @@
+pub fn convert_px(x: f32) -> f32 {
+    let size_parent_element: f32 = 16.0;
+
+    println!("Converting {} px to em, considering the parent element as {} px", x, size_parent_element);
+    return x / size_parent_element
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use dialoguer::{Select, theme::ColorfulTheme};
+use std::io;
+
+mod convert_px;
+use convert_px::convert_px;
 
 fn main() {
     println!("-----Bem-vindo ao programa CLI!-----");
 
     // Defina as opções disponíveis
-    let options = vec!["Option 1", "Option 2", "Option 3"];
+    let options = vec!["Convert px to em", "Convert px to rem", "Convert px to %"];
 
     // Crie um prompt de seleção
     let selection = Select::with_theme(&ColorfulTheme::default())
@@ -16,7 +20,33 @@ fn main() {
 
     // Execute a lógica com base na Option selecionada
     match selection {
-        0 => println!("Você escolheu a Option 1"),
+        0 => {
+
+            let mut input = String::new();
+            println!("Type the number in px to be converted to em");
+    
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Error reading console");
+
+            // let value: Result<i32, _> = input.trim().parse();
+            let value: Result<f32, _> = input.trim().parse();
+
+            match value {
+                Ok(numero) => {
+                    // Chamando a função com o valor convertido
+                    println!("O resultado é {} em", convert_px(numero));
+        
+                    // Agora você pode usar 'numero' como um inteiro
+                    // ... faça o que precisar com o inteiro aqui ...
+                }
+                Err(e) => {
+                    println!("Erro ao converter a string para inteiro: {}", e);
+                }
+            }
+
+            // println!("O resultado e {}", convert_px(value))
+        },
         1 => println!("Você escolheu a Option 2"),
         2 => println!("Você escolheu a Option 3"),
         _ => println!("Option inválida"),
